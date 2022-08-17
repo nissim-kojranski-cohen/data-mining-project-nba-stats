bd9a9bbf53f30e39fe1f5ed825592a392618be50
Nir David <oronird@gmail.com>
2022-08-17 10:20:32 +0300

    added twitter_accounts function to twitter_info.py

--- twitter_info.py
+++ twitter_info.py
@@ -1,4 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
+import csv
+import config
+import logging
 import os
 import json
 import pandas as pd
@@ -76,6 +80,63 @@ def get_all_players_id():
     df_players = df_players[df_players['Twitter'] != '']
     df_players = df_players[~df_players['Twitter'].isna()]
     return df_players
+
+
+def twitter_accounts():
+    """
+    creates a csv file with players twitter accounts url extension
+    :return: None
+    """
+    # get request of twitter info from Basketball-Reference.com
+    url = config.TWITTER_ADDRESSED_LIST
+    response = requests.get(url)
+    if response.status_code != 200:
+        logging.critical(f'request status {response} for players twitter info - NOT SUCCESSFUL!')
+    else:
+        logging.info(f'got request from players twitter info')
+
+    try:
+        logging.debug(f'parsing response url {url} for players twitter info')
+        soup = BeautifulSoup(response.text, "html.parser")
+        # fetch the table with the info
+        table = soup.find(class_='sortable stats_table')
+    except TypeError:
+        logging.error(f'request status {response} for players twitter info could not be parsed- NOT SUCCESSFUL!')
+        raise TypeError("Could not parse request")
+    except Exception as exc:
+        logging.error(f'Not defined error for request status {response} for players twitter info \n', exc)
+        raise Exception(exc)
+
+    if len(table) == 0:
+        logging.error(f"Could not find table in the requested url: {url} for players twitter info")
+        raise TypeError(f"Could not find table in the requested url: {url} for players twitter info")
+    else:
+        logging.info(f"Table returned successfully for url: {url} for players twitter info")
+
+    # create a list to store the info
+    player_twitter = []
+    # iterate over the table, each iteration is one player's info, store data to player_twitter
+    for i, row in enumerate(table.find('tbody').find_all('tr')):
+        temp_list = []
+        try:
+            for a_tag in row.find_all('a'):
+                temp_list.append(a_tag.get('href').split('/')[-1].split('.')[0])
+        except Exception as exc:
+            logging.error(f"Could not fetch data on row {i}, Details of exception: {exc}")
+        player_twitter.append(temp_list)
+
+    # save data from player_twitter to twitter_addresses.csv
+    with open(f'twitter_addresses.csv', 'w', newline='', encoding='utf-8') as file:
+        csv_writer = csv.writer(file)
+        # columns names
+        csv_writer.writerow(['player_id', 'twitter_address'])
+        for row in player_twitter:
+            try:
+                csv_writer.writerow([cell for cell in row])
+                logging.info(f"data for player {row[0]} saved successfully")
+            except Exception as exc:
+                logging.error(f"Could not save data of player {row[0]}, Details of exception: {exc}")
+
 
 if __name__ == "__main__":
     df_info = pd.DataFrame(columns=['creation_date', 'user_name', 'twitter_id', 'followers_count',

2a347ac933344b3f5f66695647ed57c85c1ba2a0
Nir David <oronird@gmail.com>
2022-08-17 16:45:35 +0300

    add twitter table insertion to to_database_tables, and aprroved it in general

--- to_database_tables.py
+++ to_database_tables.py
@@ -1,4 +1,4 @@
-import sql_config
+import pvt_data_config
 import csv
 import pymysql
 import os
@@ -11,9 +11,9 @@ logging.basicConfig(filename='nba_web_scrapping.log', encoding='utf-8', level=lo
 
 def execute_query(query):
     """ execute sql query function """
-    connection = pymysql.connect(host=sql_config.HOST,
-                                 user=sql_config.USER,
-                                 password=sql_config.PASSWORD,
+    connection = pymysql.connect(host=pvt_data_config.HOST,
+                                 user=pvt_data_config.USER,
+                                 password=pvt_data_config.PASSWORD,
                                  database=config.DATABASE_NAME)
     with connection:
         with connection.cursor() as cursor:
@@ -26,9 +26,9 @@ def execute_query(query):
 def execute_many(stmt, tup_list):
     """ execute  multiple sql queries function """
     # connecting to mysql server
-    connection = pymysql.connect(host=sql_config.HOST,
-                                 user=sql_config.USER,
-                                 password=sql_config.PASSWORD,
+    connection = pymysql.connect(host=pvt_data_config.HOST,
+                                 user=pvt_data_config.USER,
+                                 password=pvt_data_config.PASSWORD,
                                  database=config.DATABASE_NAME)
 
     # execute query
@@ -36,6 +36,37 @@ def execute_many(stmt, tup_list):
         with connection.cursor() as cursor:
             cursor.executemany(stmt, tup_list)
             connection.commit()
+
+
+def generate_row(row_data, varchar_cols=[], omited_cols=[]):
+    # creating a list for each row by its value type
+    row_data_list = []
+    for key in row_data:
+        if key in omited_cols:
+            pass
+        elif key in varchar_cols:
+            row_data_list.append(row_data[key])
+        elif row_data[key] == '' or row_data[key] == 'Undrafted':
+            row_data_list.append(None)
+        else:
+            row_data_list.append(float(row_data[key]))
+    return row_data_list
+
+
+def get_db_rows(table_name, col_names):
+    in_db = execute_query(f"""SELECT {', '.join(col_names)} FROM {table_name}""")
+    return in_db
+
+
+def insert_tuple_to_db(tup_list, table_name):
+    # check if there is new data to insert
+    if len(tup_list) > 0:
+        col_num = '%s, ' * len(tup_list[0])
+        stmt = f"INSERT INTO {table_name} VALUES ({col_num[:-2]})"
+        execute_many(stmt, tup_list)
+        logging.info(f'Data inserted to {table_name} table')
+    else:
+        logging.info(f'No new data to insert to {table_name} table')
 
 
 def to_stats_table(filename):
@@ -54,43 +85,24 @@ def to_stats_table(filename):
         reader = csv.DictReader(file)
         # store each dictionary (representing a player) to tupel list
         tup_list = []
+        # query to check if sample is in database
+        in_db = get_db_rows(table_name=f'stats_{type_of_stat}', col_names=['player_id', 'season'])
+        varchar_cols = ['player_id', 'pos', 'team_id']
+        omited_cols = ['', 'player', 'age']
         for index, player_data in enumerate(reader):
-            varchar_cols = ['player_id', 'pos', 'team_id']
-            omited_cols = ['', 'player', 'age']
             # creating a list for each row by its value type
-            row_data = []
-            for key in player_data:
-                if key in omited_cols:
-                    pass
-                elif key in varchar_cols:
-                    row_data.append(player_data[key])
-                elif player_data[key] == '':
-                    row_data.append(None)
-                else:
-                    row_data.append(float(player_data[key]))
+            row_data = generate_row(player_data, varchar_cols=varchar_cols, omited_cols=omited_cols)
             # insert year column
             row_data.insert(3, year)
-            # query to check if sample is in database
-            in_db = execute_query(f"""SELECT player_id, season 
-                                 FROM stats_{type_of_stat} 
-                                 WHERE player_id='{row_data[0]}' 
-                                 AND season={row_data[3]}""")
             # checking if sample is in database, only new data is stored
-            if len(in_db) > 0:
+            if (row_data[0], row_data[3]) in in_db:
                 # the sample exists in the database
                 pass
             else:
                 # saving the row data to tuple and storing in tup_list
                 tup_list.append(tuple(row_data))
 
-        # check if there is new data to insert
-        if len(tup_list) > 0:
-            col_num = '%s, ' * len(tup_list[0])
-            stmt = f"INSERT INTO stats_{type_of_stat} VALUES ({col_num[:-2]})"
-            execute_many(stmt, tup_list)
-            logging.info(f'Data inserted to stats_{type_of_stat} table')
-        else:
-            logging.info(f'NO new data to insert to stats_{type_of_stat} table')
+        insert_tuple_to_db(tup_list, table_name=f'stats_{type_of_stat}')
 
 
 def to_players_table(filename):
@@ -110,39 +122,55 @@ def to_players_table(filename):
         reader = csv.DictReader(file)
         # store each dictionary (representing a player) to tuple list
         tup_list = []
+        # query to check if sample is in database
+        in_db = get_db_rows(table_name=table_name, col_names=['player_id'])
         varchar_cols = ['player_id', 'team', 'college', 'country', 'player']
         omited_cols = ['']
         for index, player_data in enumerate(reader):
             # creating a list for each row by its value type
-            row_data = []
-            for key in player_data:
-                if key in omited_cols:
-                    pass
-                elif key in varchar_cols:
-                    row_data.append(player_data[key])
-                elif player_data[key] == '' or player_data[key] == 'Undrafted':
-                    row_data.append(None)
-                else:
-                    row_data.append(float(player_data[key]))
-            # query to check if sample is in database
-            in_db = execute_query(f"""SELECT player_id 
-                                             FROM {table_name} 
-                                             WHERE player_id='{row_data[0]}'""")
+            row_data = generate_row(player_data, varchar_cols=varchar_cols, omited_cols=omited_cols)
+
             # checking if sample is in database, only new data is stored
-            if len(in_db) > 0:
+            if (row_data[0],) in in_db:
                 pass
             else:
                 # saving the row data to tuple and storing in tup_list
                 tup_list.append(tuple(row_data))
 
-        # check if there is new data to insert
-        if len(tup_list) > 0:
-            col_num = '%s, ' * len(tup_list[0])
-            stmt = f"INSERT INTO {table_name} VALUES ({col_num[:-2]})"
-            execute_many(stmt, tup_list)
-            logging.info(f'Data inserted to {table_name} table')
-        else:
-            logging.info(f'NO new data to insert to {table_name} table')
+        insert_tuple_to_db(tup_list, table_name=table_name)
+
+
+def to_twitter_table(filename):
+    """
+    takes csv file and insert it to the appropriate mysql table in nba_data db
+    :param filename: string representing the file name
+    :return: None
+    """
+    with open(filename, encoding='utf-8') as file:
+        # extract year and stat type from file name
+        table_name = filename.split('.')[0]
+        logging.info(f'finshed reading {filename}')
+
+        # read the csv
+        reader = csv.DictReader(file)
+        # store each dictionary (representing a player) to tuple list
+        tup_list = []
+        # query to check if sample is in database
+        in_db = get_db_rows(table_name=table_name, col_names=['player_id'])
+        varchar_cols = ['player_id', 'creation_date', 'user_name', 'description']
+        omited_cols = ['']
+        for index, player_data in enumerate(reader):
+            # creating a list for each row by its value type
+            row_data = generate_row(player_data, varchar_cols=varchar_cols, omited_cols=omited_cols)
+
+            # checking if sample is in database, only new data is stored
+            if (row_data[0]) in in_db:
+                pass
+            else:
+                # saving the row data to tuple and storing in tup_list
+                tup_list.append(tuple(row_data))
+
+        insert_tuple_to_db(tup_list, table_name=table_name)
 
 
 def to_teams_table():
@@ -185,21 +213,22 @@ def to_teams_table():
     logging.info(f'Data inserted to teams table')
 
 
+def write_file_types(file_list, db_func, startswith, endswith='.csv'):
+    for file in file_list:
+        # insert all stats csv files to mysql tables
+        if file.endswith(endswith) and file.startswith(startswith):
+            db_func(file)
+            logging.info(f'Data for {file} inserted succesfully')
+
+
 def write_to_tables():
     current_path = pathlib.Path().resolve()
     # list of all files in current directory
     onlyfiles = [f for f in os.listdir(current_path) if os.path.isfile(os.path.join(current_path, f))]
-    for file in onlyfiles:
-        # insert all stats csv files to mysql tables
-        if file.endswith('.csv') and file.startswith('sample'):
-            to_stats_table(file)
-            logging.info(f'Data for {file} inserted succesfully')
 
-    for file in onlyfiles:
-        # insert all player info csv files to mysql tables
-        if file.endswith('.csv') and file.startswith('players'):
-            to_players_table(file)
-            logging.info(f'Data for {file} inserted succesfully')
+    write_file_types(onlyfiles, db_func=to_stats_table, startswith='sample', endswith='.csv')
+    write_file_types(onlyfiles, db_func=to_players_table, startswith='players', endswith='.csv')
+    write_file_types(onlyfiles, db_func=to_twitter_table, startswith='twitter_details', endswith='.csv')
     to_teams_table()
 
 
